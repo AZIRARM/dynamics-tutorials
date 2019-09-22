ad41c43c4bbb94f25b280d69a73540d1756b2d98
azirar mhamed <azirarm@gmail.com>
2019-09-23 00:19:32 +0200

    tutorial 2 : integration firebase et déploiement de l'api dans le cloud

---
+++ tuto2/discussion/functions/index.js
@@ -0,0 +1,18 @@
+const functions = require('firebase-functions');
+
+var express = require('express');
+var api = express();
+var port = 9000;
+
+api.get('/', function(req, resp) {
+    resp.setHeader('Content-Type','text/plain');
+
+    resp.send("Api Restfull pour gérer une discussion simple");
+});
+
+api.listen(port, function() {
+    console.log("Le serveur discussion api est encours d'exécution");
+});
+
+//on exporte notre api dans firebase
+exports.api = functions.https.onRequest(api);
